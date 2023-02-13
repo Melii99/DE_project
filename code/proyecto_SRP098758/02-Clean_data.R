e51c0fbe29fad7e8f9d5d7558c987e2d65f3e0db
Melissa <yuukiakiamano@gmail.com>
2023-02-12 21:19:42 -0600

    Script para limpieza y normalización de datos

---
+++ code/proyecto_SRP098758/02-Clean_data.R
@@ -0,0 +1,38 @@
+############################## Limpieza de los datos ###########################
+
+## Guardamos el objeto original
+rse_gene_nofilter <- rse_gene
+
+## Creamos un histograma con la proporcion de genes
+hist(rse_gene$assigned_gene_prop)
+
+## Eliminamos las muestras malas
+table(rse_gene$assigned_gene_prop < 0.3)
+rse_gene <- rse_gene[, rse_gene$assigned_gene_prop > 0.3]
+
+
+## Niveles medios de expresión de los genes en las muestras
+gene_means <- rowMeans(assay(rse_gene, "counts"))
+summary(gene_means)
+
+## Eliminamos genes
+rse_gene <- rse_gene[gene_means > 0.1, ]
+
+## Dimensiones iniciales
+dim(rse_gene_nofilter)
+## Dimensiones finales
+dim(rse_gene)
+
+## Porcentaje de genes retenidos
+round(nrow(rse_gene) / nrow(rse_gene_nofilter) * 100, 2)
+
+########################## Normalización de datos ##############################
+
+library("edgeR")
+
+dge <- DGEList(
+  counts = assay(rse_gene, "counts"),
+  genes = rowData(rse_gene)
+)
+
+dge <- calcNormFactors(dge)
